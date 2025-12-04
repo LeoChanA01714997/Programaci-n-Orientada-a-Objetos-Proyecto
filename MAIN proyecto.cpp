3de3fcebf70198b12587ec81c9d85e7ea8685feb
LeoChanA01714997 <A01714997@tec.mx>
2025-12-04 01:05:11 -0600

    MAIN proyecto.cpp

--- MAIN proyecto.cpp
+++ MAIN proyecto.cpp
@@ -1,34 +1,18 @@
-int main() {
+#include "Sistema.hpp"
 
-    // 1. Vector2D
-    Vector2D v1(3.0, 4.0);
-    std::cout << "Magnitud: " << v1.magnitud() << "\n";
-    v1.imprimir();
+int main(){
+    Sistema sis(0.01);
 
-    // 2. CuerpoCeleste
-    CuerpoCeleste planeta(
-        "Planeta Azul",
-        5.9e24,
-        Vector2D(1.0e6, 2.0e6),
-        Vector2D(500.0, -200.0)
-    );
+    sis.agregarEstrella( Estrella("Sol",    1000, Vector2D(0,0),   Vector2D(0,0)) );
+    sis.agregarPlaneta ( Planeta ("Tierra", 1.0,  Vector2D(1,0),   Vector2D(0,1)) );
+    sis.agregarSatelite( Satelite("Luna",   0.01, Vector2D(1.2,0), Vector2D(0,1.2)) );
 
-    planeta.imprimirDatos();
-    planeta.mover(1000.0, 500.0);
-    planeta.actualizarVelocidad(Vector2D(0.2, -0.1), 5.0);
-    planeta.actualizarPosicion(5.0);
+    Planeta mercurio("Mercurio", 0.055, Vector2D(0.4,0), Vector2D(0,1.6));
+    sis.agregarExterno(&mercurio);
 
-    std::cout << "Después de actualizar:\n";
-    planeta.imprimirDatos();
-
-    // 3. RenderConfig
-    RenderConfig config(800, 800, 1e6);
-    config.imprimir();
-
-    auto pantalla = config.convertir(Vector2D(2.0e6, -1.0e6));
-    std::cout << "Coordenadas convertidas: (" 
-              << pantalla.first << ", " << pantalla.second << ")\n";
-
+    for(int i=0;i<300;++i) sis.paso();
+    sis.mostrar();
     return 0;
 }
+
 
