24fd6293b244fc77178af5d9f94f038fb18c77e8
LeoChanA01714997 <A01714997@tec.mx>
2025-12-04 01:08:25 -0600

    MAIN proyecto.cpp

--- MAIN proyecto.cpp
+++ MAIN proyecto.cpp
@@ -1,18 +1,14 @@
 #include "Sistema.hpp"
 
 int main(){
-    Sistema sis(0.01);
+    Estrella sol("Sol", 1000, Vector2D(0,0), Vector2D(0,0));
+    Sistema sis(0.01, sol);
 
-    sis.agregarEstrella( Estrella("Sol",    1000, Vector2D(0,0),   Vector2D(0,0)) );
-    sis.agregarPlaneta ( Planeta ("Tierra", 1.0,  Vector2D(1,0),   Vector2D(0,1)) );
-    sis.agregarSatelite( Satelite("Luna",   0.01, Vector2D(1.2,0), Vector2D(0,1.2)) );
-
-    Planeta mercurio("Mercurio", 0.055, Vector2D(0.4,0), Vector2D(0,1.6));
-    sis.agregarExterno(&mercurio);
+    sis.agregarPlaneta( Planeta("Tierra", 1.0, Vector2D(1,0), Vector2D(0,1.0)) );
+    sis.agregarPlaneta( Planeta("Mercurio", 0.055, Vector2D(0.4,0), Vector2D(0,1.6)) );
 
     for(int i=0;i<300;++i) sis.paso();
     sis.mostrar();
     return 0;
 }
-
 
