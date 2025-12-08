faaa2c8bfbfce3cb2ee80ecb75efb0536751a65b
LeoChanA01714997 <A01714997@tec.mx>
2025-12-07 21:23:54 -0600

    Sistema.cpp

--- Sistema.cpp
+++ Sistema.cpp
@@ -1,26 +1,32 @@
 #include "Sistema.hpp"
-#include <algorithm>
 
 Sistema::Sistema(double paso, const Estrella& estrella)
-: dt(paso), sol(estrella) {}
+    : dt(paso), sol(estrella) {}
 
-void Sistema::agregarPlaneta(const Planeta& p){
+void Sistema::agregarPlaneta(const Planeta& p) {
     planetas.push_back(p);
 }
 
-void Sistema::paso(){
+void Sistema::paso() {
     const double GM = 1.0 * sol.getMasa();
-    for(auto& p : planetas){
-        Vector2D dr(p.r().x() - sol.r().x(), p.r().y() - sol.r().y());
-        double d = dr.norm(); if(d < 1e-9) d = 1e-9;
-        double k = -GM / (d*d*d);
-        p.acelerar(k*dr.x(), k*dr.y(), dt); 
-        p.integrar(dt);                      
+    for (auto& p : planetas) {
+        Vector2D dr(p.r().x() - sol.r().x(),
+                    p.r().y() - sol.r().y());
+        double d = dr.norm();
+        if (d < 1e-9) d = 1e-9;
+        double k = -GM / (d * d * d);
+
+        p.acelerar(k * dr.x(), k * dr.y(), dt);
+        p.integrar(dt);
     }
 }
 
-void Sistema::mostrar(){
+void Sistema::mostrar() const {
     std::cout << "Estrella: " << sol.info() << "\n";
-    for(const auto& p : planetas) std::cout << "Planeta : " << p.info() << "\n";
+    for (const auto& p : planetas) {
+        std::cout << "Planeta : " << p.info() << "\n";
+    }
     std::cout << "\n";
 }
+
+
