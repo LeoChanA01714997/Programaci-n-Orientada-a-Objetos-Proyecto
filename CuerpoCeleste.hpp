3a21953c8734072beeac340e007a6a857729d7a3
LeoChanA01714997 <A01714997@tec.mx>
2025-12-07 21:21:29 -0600

    CuerpoCeleste.hpp

--- CuerpoCeleste.hpp
+++ CuerpoCeleste.hpp
@@ -8,15 +8,17 @@ protected:
     double masa;
     Vector2D pos;
     Vector2D vel;
+
 public:
     CuerpoCeleste(std::string n, double m, Vector2D p, Vector2D v);
     virtual ~CuerpoCeleste() = default;
 
-    void acelerar(double ax, double ay, double dt); 
-    virtual void integrar(double dt);               
+    void acelerar(double ax, double ay, double dt);
+    virtual void integrar(double dt);
 
     std::string info() const;
     const Vector2D& r() const { return pos; }
     double getMasa() const { return masa; }
-};
 
+    const std::string& getNombre() const { return nombre; }
+};
