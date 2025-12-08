66e57d102573d67ae3655a9150a1c2fa48f3476f
LeoChanA01714997 <A01714997@tec.mx>
2025-12-07 21:21:48 -0600

    CuerpoCeleste.cpp

--- CuerpoCeleste.cpp
+++ CuerpoCeleste.cpp
@@ -1,15 +1,17 @@
 #include "CuerpoCeleste.hpp"
 #include <sstream>
 
-CuerpoCeleste::CuerpoCeleste(std::string n,double m,Vector2D p,Vector2D v)
-: nombre(std::move(n)), masa(m), pos(p), vel(v) {}
+CuerpoCeleste::CuerpoCeleste(std::string n, double m, Vector2D p, Vector2D v)
+    : nombre(std::move(n)), masa(m), pos(p), vel(v) {}
 
-void CuerpoCeleste::acelerar(double ax,double ay,double dt){
-    vel = vel + Vector2D(ax*dt, ay*dt);
+void CuerpoCeleste::acelerar(double ax, double ay, double dt) {
+    vel = vel + Vector2D(ax * dt, ay * dt);
 }
-void CuerpoCeleste::integrar(double dt){
-    pos = pos + vel*dt;
+
+void CuerpoCeleste::integrar(double dt) {
+    pos = pos + vel * dt;
 }
+
 std::string CuerpoCeleste::info() const {
     std::ostringstream os;
     os << nombre << " m=" << masa
