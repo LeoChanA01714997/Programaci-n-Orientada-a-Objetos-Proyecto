ec5f1b5c69f6136f4a06939458bdb6ec781b548e
LeoChanA01714997 <A01714997@tec.mx>
2025-12-07 21:22:47 -0600

    Satelite.hpp

--- Satelite.hpp
+++ Satelite.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "CuerpoCeleste.hpp"
+
 class Satelite : public CuerpoCeleste {
-public: using CuerpoCeleste::CuerpoCeleste;
+public:
+    using CuerpoCeleste::CuerpoCeleste;
 };
