11fee9f929a7eef11269b1bfd71002cf7da1315e
LeoChanA01714997 <A01714997@tec.mx>
2025-12-07 21:23:09 -0600

    Sistema.hpp

--- Sistema.hpp
+++ Sistema.hpp
@@ -7,11 +7,11 @@
 class Sistema {
 public:
     double dt;
-    Estrella sol;                     
-    std::vector<Planeta> planetas;   
+    Estrella sol;
+    std::vector<Planeta> planetas;
 
     Sistema(double paso, const Estrella& estrella);
     void agregarPlaneta(const Planeta& p);
-    void paso();   
-    void mostrar();
+    void paso();
+    void mostrar() const;
 };
