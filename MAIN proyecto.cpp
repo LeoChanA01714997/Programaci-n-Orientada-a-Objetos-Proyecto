93b1d182737b2b948ceb02c8728ae5bcbf3470f0
LeoChanA01714997 <A01714997@tec.mx>
2025-12-07 21:24:18 -0600

    Update MAIN proyecto.cpp

--- MAIN proyecto.cpp
+++ MAIN proyecto.cpp
@@ -1,14 +1,35 @@
+#include <iostream>
+#include <iomanip>
 #include "Sistema.hpp"
 
-int main(){
-    Estrella sol("Sol", 1000, Vector2D(0,0), Vector2D(0,0));
-    Sistema sis(0.01, sol);
+int main() {
+    //estrella posicionada en el origen
+    Estrella sol("Sol", 1000.0, Vector2D(0.0, 0.0), Vector2D(0.0, 0.0));
+    Sistema sis(0.01, sol);   // dt = 0.01
 
-    sis.agregarPlaneta( Planeta("Tierra", 1.0, Vector2D(1,0), Vector2D(0,1.0)) );
-    sis.agregarPlaneta( Planeta("Mercurio", 0.055, Vector2D(0.4,0), Vector2D(0,1.6)) );
+    //planetas
+    sis.agregarPlaneta(
+        Planeta("Tierra", 1.0, Vector2D(1.0, 0.0), Vector2D(0.0, 1.0))
+    );
+    sis.agregarPlaneta(
+        Planeta("Mercurio", 0.055, Vector2D(0.4, 0.0), Vector2D(0.0, 1.6))
+    );
+    //numero de pasos de integracion y cada cuantos pasos se desea imprimir (modificable)
+    const int pasosTotales = 5000; 
+    const int imprimirCada = 500;  
 
-    for(int i=0;i<300;++i) sis.paso();
-    sis.mostrar();
+    std::cout << std::fixed << std::setprecision(3);
+
+    for (int i = 0; i < pasosTotales; ++i) {
+        sis.paso();
+
+        if ((i + 1) % imprimirCada == 0) {
+            double t = (i + 1) * sis.dt;
+            std::cout << "t = " << t << "\n";
+            sis.mostrar();
+        }
+    }
+
+    std::cout << "Simulacion terminada \n";
     return 0;
 }
-
