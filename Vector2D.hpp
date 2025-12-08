8846a512ba24cde4e00908089a654243cba36a2a
LeoChanA01714997 <A01714997@tec.mx>
2025-12-07 21:19:53 -0600

    Vector2D.hpp

--- Vector2D.hpp
+++ Vector2D.hpp
@@ -2,14 +2,18 @@
 #include <cmath>
 
 class Vector2D {
-protected:                
+protected:
     double x_, y_;
+
 public:
-    Vector2D(double x=0, double y=0);
-    double x() const;  double y() const;
-    void set_x(double x); void set_y(double y);
+    Vector2D(double x = 0, double y = 0);
+    double x() const;
+    double y() const;
+    void set_x(double x);
+    void set_y(double y);
     double norm() const;
 
     Vector2D operator+(const Vector2D& o) const;
     Vector2D operator*(double s) const;
 };
+
